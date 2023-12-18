088f56b24623e8a8fb47e64fbbabc47e46e2cdb9
Michael Goehrig <jan.michael.goehrig@pexon-consulting.de>
2023-12-18 19:53:43 +0100

    fix: removes testing error

--- utils/build-request.ts
+++ utils/build-request.ts
@@ -3,12 +3,13 @@ import type {Conversation} from "~/stores/converstation-store";
 export default function (question: string, { email, session, conversation}: Conversation) {
     return  {
         question: question,
-        // chat_history: conversation.map(prompt => ({
-        //     inputs: { question: prompt.question },
-        //     outputs: { answer: prompt.answer.value },
-        //
-        // })),
-        chat_history: {},
+        chat_history: conversation.map(prompt => ({
+            inputs: { question: prompt.question },
+            outputs: { answer: prompt.answer.value },
+
+        })),
+        // for testing: triggering error
+        // chat_history: {},
         email:  email,
         session: session,
     }
